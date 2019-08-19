593351d88975f4bce8fdb8fccec099bcbc8e71b2
Raiz <mohamed_raiz@hotmail.com>
2019-08-19 23:18:17 +0800

    Further updates

--- learnRestAssuredAPI/src/main/java/learningRestAssured/basicsPostPart2.java
+++ learnRestAssuredAPI/src/main/java/learningRestAssured/basicsPostPart2.java
@@ -1,43 +1,46 @@
 package learningRestAssured;
 
+import files.payload;
+import files.resources;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 public class basicsPostPart2  {
+
+    Properties prop = new Properties();
+   @BeforeTest
+    public void getData() throws IOException {
+
+
+       FileInputStream fis = new FileInputStream("/Users/mohamedraiz/Desktop/Coding/RestAssuredAPI/learnRestAssuredAPI/src/main/java/files/env.properties");
+       prop.load(fis);
+       prop.getProperty("HOST");
+   }
+
     @Test
     public void postAndDeleteData() {
 
         //baseUrl or Hostname:
-        RestAssured.baseURI = "http://216.10.245.166";
+        RestAssured.baseURI = prop.getProperty("HOST");
         Response res;
 
-        String bodyContent = "{\n" +
-                "    \"location\": {\n" +
-                "        \"lat\": -38.383494,\n" +
-                "        \"lng\": 33.427362\n" +
-                "    },\n" +
-                "    \"accuracy\": 50,\n" +
-                "    \"name\": \"Frontline house\",\n" +
-                "    \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                "    \"address\": \"29, side layout, cohen 09\",\n" +
-                "    \"types\": [\n" +
-                "        \"shoe park\",\n" +
-                "        \"shop\"\n" +
-                "    ],\n" +
-                "    \"website\": \"http://google.com\",\n" +
-                "    \"language\": \"French-IN\"\n" +
-                "}";
+
         res = given().
-                queryParam("key","qaclick123").
-                body(bodyContent).
+                queryParam("key", prop.getProperty("KEY ")).
+                body(payload.getPostData()).
                 when().
-                post("/maps/api/place/add/json").
+                post(resources.placePostData()).
                 then().assertThat().statusCode(200).and().
                 contentType(ContentType.JSON).and().
                 body("status",equalTo("OK")).and().
